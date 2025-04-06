5def5091a830d4705607c947e9eae2f16aab0a6c
Kushang Agarwal <kushang.agarwal@students.iiit.ac.in>
2025-04-06 15:55:54 +0530

    added count_loss in the loss functions and also tied embedding of new hmpn for motifdist to the encoders embedding space

--- hgraph/hgnn_metal.py
+++ hgraph/hgnn_metal.py
@@ -86,6 +86,7 @@ class HierVAEMetalDist(nn.Module):
         self.decoder=HierMPNDecoderMetalDist(args.vocab, args.atom_vocab, args.rnn_type, args.embed_size, args.hidden_size, args.latent_size, args.diterT, args.diterG, args.dropout)
         self.encoder.tie_embedding(self.decoder.hmpn)
         self.encoder.tie_embedding(self.decoder.hmpn_distances)
+        self.encoder.tie_embedding(self.decoder.hmpn_motifdist)
         self.latent_size = args.latent_size
 
 
@@ -121,8 +122,9 @@ class HierVAEMetalDist(nn.Module):
         root_vecs, root_kl = self.rsample(root_vecs, self.R_mean, self.R_var, perturb=False)
         return self.decoder.decode((root_vecs, root_vecs, root_vecs), greedy=True, max_decode_step=150)
     
-    def forward(self, graphs, tensors, orders, beta, epoch_no, perturb_z=True):
+    def forward(self, graphs, tensors, orders, ligand_counts, beta, epoch_no, perturb_z=True):
         tree_tensors, graph_tensors = tensors = make_cpu_dist(tensors)
+
 
         root_vecs, tree_vecs, _, graph_vecs = self.encoder(tree_tensors, graph_tensors)
 
@@ -133,9 +135,9 @@ class HierVAEMetalDist(nn.Module):
         # print("encoding: ",root_vecs)
 
         # try:
-        loss,wacc,iacc,tacc,sacc,dist_loss, dist_loss_tree = self.decoder((root_vecs, root_vecs, root_vecs), graphs, tensors, orders, epoch_no)
+        loss,wacc,iacc,tacc,sacc,dist_loss, dist_loss_tree, count_loss = self.decoder((root_vecs, root_vecs, root_vecs), graphs, tensors, orders, ligand_counts, epoch_no)
         # except Exception as e:
         #     print("Error in decoder")
         #     print(e)
         #     return 0,0,0,0,0,0
-        return loss + 1 * kl_div, kl_div.item(), wacc, iacc, tacc, sacc, dist_loss, dist_loss_tree
+        return loss + 1 * kl_div, kl_div.item(), wacc, iacc, tacc, sacc, dist_loss, dist_loss_tree, 0.5*count_loss
